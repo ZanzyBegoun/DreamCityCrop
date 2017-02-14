78964f81e519a47ef212a0b327896f76bf5d98a8
ZanzyBegoun <begouna@mx.lakeforest.edu>
2017-02-13 21:31:47 -0600

    Add files via upload
    Added sketch, library, and index

---
+++ sketch.js
@@ -0,0 +1,294 @@
+
+
+function setup() {
+    createCanvas(600, 600);
+   
+    
+}
+
+
+
+function draw() {
+noStroke();
+    
+    fill(125);
+    rect(0, 0, 600, 600);
+    fill(77, 143, 116);
+    rect(0, 0, 108, 75);
+    
+    fill(44,46,31);
+    beginShape();
+    vertex(0, 183);
+    vertex(36, 183);
+    vertex(98, 95);
+    vertex(152, 95);
+    vertex(161, 0);
+    vertex(99, 0);
+    vertex(96, 69);
+    vertex(0, 75);
+    endShape(CLOSE);
+    
+    fill(200, 200, 187);
+    rect(0, 278, 102, 52);
+    
+    fill(144,171,155);
+    rect(286, 0, 59, 272);
+    
+    fill(113,164,155);
+    rect(237, 0, 63, 142);
+    
+    fill(88,151,136);
+    rect(212.7, 0, 32, 145);
+    
+    fill(84, 119, 110);
+    rect(189, 0, 42, 136);
+    
+    fill(72, 113, 93);
+    rect(173.8, 0, 30, 131.6);
+    
+    fill(52, 65, 52);
+    rect(143, 0, 34, 117);
+    
+    fill(128, 155, 142);
+    rect(244, 140, 42, 160);
+    
+    fill(144, 176, 169);
+    rect(216, 140, 36, 190);
+    
+    fill(200, 200, 187);
+    rect(182, 129, 34, 225);
+    
+    fill(95, 123, 118);
+    rect(145, 150, 37, 230);
+    
+    fill(85, 138, 119);
+    rect(102, 164, 43, 258);
+    
+    fill(92, 116, 100);
+    rect(64, 177, 44, 137);
+    
+    fill(78, 95, 73);
+    rect(36, 183, 28, 104);
+    
+    fill(72, 82, 62);
+    rect(14, 183, 22, 95);
+    
+    fill(96, 123, 109);
+    rect(0, 183, 14, 110);
+    
+    fill(70, 81, 59);
+    beginShape();
+    vertex(34, 186);
+    vertex(100, 95);
+    vertex(149, 95);
+    vertex(83, 187);
+    endShape(CLOSE);
+    
+    fill(78, 152, 138);
+    beginShape();
+    vertex(78, 187);
+    vertex(207, 140);
+    vertex(146, 95);
+    endShape(CLOSE);
+ 
+   
+    fill(130, 167, 158);
+    rect(36, 330, 66, 176);
+    
+    fill(130, 132, 117);
+    rect(0, 332, 36, 170);
+    
+    fill(218, 194, 164);
+    rect(36, 506, 66, 94);
+    
+    fill(149, 189, 173);
+    rect(0, 502, 36, 98);
+    
+    fill(201, 188, 166);
+    beginShape();
+    vertex(344, 600);
+    vertex(467, 600);
+    vertex(429, 0);
+    vertex(345, 0);
+    endShape(CLOSE);
+   
+    fill(185, 195, 184);
+    beginShape();
+    vertex(600, 452);
+    vertex(539, 492);
+    vertex(454.4, 422);
+    vertex(427, 0);
+    vertex(600, 0);
+    endShape(CLOSE);
+    
+    
+    //house
+    
+    fill(236, 231, 214);
+    rect(102, 444, 243, 156);
+    
+    fill(180, 137, 110);
+	rect(93, 444, 61, 55);
+    
+	fill(214, 193, 163);
+    rect(154, 499, 71, 98);
+    
+    fill(182, 187, 174);
+	rect(225, 499, 60, 98);
+
+    fill(202, 176, 150);
+    beginShape();
+    vertex(345,444);
+    vertex(217,334);
+    vertex(87,424);
+    vertex(87,444);
+    endShape(CLOSE);
+    
+    fill(162, 123, 94);
+    beginShape();
+    vertex(239.5,353.4);
+    vertex(258.9,370);
+    vertex(165,370);
+    vertex(189.5,353); 
+    endShape(CLOSE);
+    
+    beginShape();
+    vertex(137.6,389);
+    vertex(278.2,389);
+    vertex(300,405.3);
+    vertex(111.4,407.1);
+    endShape(CLOSE);
+    
+    
+    beginShape();
+    vertex(319.7,422.3);
+    vertex(342.3,441.7);
+    vertex(87,437.5);
+    vertex(87,422);
+    vertex(98.8,415.9);
+    endShape(CLOSE);
+    
+    fill(182, 184, 164);
+    beginShape();
+    vertex(600,452);
+    vertex(539,492);
+    vertex(454,422);
+    vertex(460,511.5);
+    vertex(466,601);
+    vertex(600,600);
+    endShape(CLOSE);
+    
+    fill(114, 165, 157);
+    rect(549, 506, 51, 95);
+    
+    fill(114, 165, 157);
+    beginShape();
+    vertex(539, 492);
+    vertex(460, 512);
+    vertex(454, 422);
+    endShape(CLOSE);
+    
+    fill(132, 156, 143);
+    beginShape();
+    vertex(345,443);
+    vertex(446,311);
+    vertex(345,206);
+    vertex(216,333);
+    endShape(CLOSE);
+    
+    fill(93, 145, 123);
+    beginShape();
+    vertex (345,206);
+    vertex (331,338);
+    vertex (241.3,306.5);
+    endShape(CLOSE);
+    
+    beginShape();
+    vertex(345,441);
+    vertex(345,338);
+    vertex(446,311); 
+    endShape(CLOSE);
+    
+    fill(237, 225, 208);
+    rect(496, 14, 43, 60);
+    rect(496, 173, 46, 66);
+    rect(496, 318, 46, 46);
+    rect(504, 420, 36, 36);
+    
+    fill(102, 157, 144);
+    beginShape();
+    vertex(482, 416);
+    vertex(566, 416);
+    vertex(524, 370); 
+    endShape(CLOSE);
+    
+    beginShape();
+    vertex(482, 318);
+    vertex(566, 318);
+    vertex(539, 254); 
+    endShape(CLOSE);
+    
+    beginShape();
+    vertex(474, 173);
+    vertex(574, 173);
+    vertex(510, 108); 
+    endShape(CLOSE);
+    
+    beginShape();
+    vertex(467, 0);
+    vertex(482, 14);
+    vertex(549, 14); 
+    vertex(558, 0); 
+    endShape(CLOSE);
+    
+    beginShape();
+    vertex(600, 272);
+    vertex(584, 300);
+    vertex(600, 300);  
+    endShape(CLOSE);
+    
+    beginShape();
+    vertex(600, 120);
+    vertex(574, 141);
+    vertex(600, 149);  
+    endShape(CLOSE);
+    
+    beginShape();
+    vertex(600, 0);
+    vertex(587, 14);
+    vertex(600, 14);  
+    endShape(CLOSE);
+    
+    
+    //edits added at end 
+    
+    fill(200, 200, 187);
+    beginShape();
+    vertex(102,314);
+    vertex(60,283);
+    vertex(0,272);
+    vertex(0, 317.7);
+    endShape(CLOSE);
+    
+    fill(32, 76, 68);
+    rect(0, 22, 98, 23);
+    
+    fill(50, 56, 42);
+    beginShape();
+    vertex(108,95);
+    vertex(93,95);
+    vertex(30,187);
+    vertex(36,187);
+    endShape(CLOSE);
+    
+    fill(80, 137, 126);
+    rect(549, 506, 30, 91);
+    
+    fill(80, 137, 126);
+    beginShape();
+    vertec(123,170);
+    vertex(216,129);
+    vertex(165,110);
+    
+}
+
